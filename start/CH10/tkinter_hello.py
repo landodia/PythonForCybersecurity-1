b0a9037baf3b7d68cabca667c6aef519c9bb966b
landodia <landon.diana@student.greenriver.edu>
2026-03-22 17:45:42 -0700

    Update tkinter_hello.py
    Added a greeting function and user input handling.

--- start/CH10/tkinter_hello.py
+++ start/CH10/tkinter_hello.py
@@ -1,5 +1,5 @@
 # First tkinter script
-# Create by 
+# Create by Diana Landon 3/22/26
 
 # Import tkinter
 
@@ -8,3 +8,22 @@
 # Add widgets
 
 # Enter the main event loop
+
+import tkinter
+
+def greet_user():
+  name= name_entry.get()
+  result_label.config(text="Hello, + name + "Today will be a great day!")
+
+root = tkinter.Tk()
+root.title("Hello, Tkinter!")
+
+tkinter.Label(root, text="What is your name?").pack()
+name_entry = tkinter.Entry(root)
+name_entry.pack()
+tkinter.Button(root, text="submit", command=greet_user).pack()
+
+result_label = tkinter.Label(root, text="")
+result_label.pack()
+
+root.mainloop()
